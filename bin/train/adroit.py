0aba28d0ac3b5150decf3d7728e31dfe061becb2
Chang Hahn <changhoon.hahn@princeton.edu>
2021-12-16 10:15:01 -0500

    script to validate trained anpe with p-p plot

--- bin/train/adroit.py
+++ bin/train/adroit.py
@@ -42,11 +42,56 @@ def anpe(sample, itrain, nhidden, nblocks, gpu=False):
     return None 
 
 
+def valid(sample, itrain, nhidden, nblocks):
+    ''' validate trained ANPE 
+    '''
+    cntnt = '\n'.join([
+        "#!/bin/bash", 
+        "#SBATCH -J v.anpe.%s.%ix%i.%i" % (sample, nhidden, nblocks, itrain), 
+        "#SBATCH --partition=general",
+        "#SBATCH --time=11:59:59", 
+        "#SBATCH --export=ALL", 
+        "#SBATCH --output=o/valid.anpe.%s.%ix%i.%i.o" % (sample, nhidden, nblocks, itrain), 
+        "#SBATCH --mail-type=all", 
+        "#SBATCH --mail-user=chhahn@princeton.edu", 
+        "", 
+        'now=$(date +"%T")', 
+        'echo "start time ... $now"', 
+        "", 
+        "source ~/.bashrc", 
+        "conda activate torch-env", 
+        "",
+        "python /home/chhahn/projects/SEDflow/bin/train/valid.py %s %i %i %i" % (sample, itrain, nhidden, nblocks),
+        "",
+        'now=$(date +"%T")', 
+        'echo "end time ... $now"', 
+        ""]) 
+
+    # create the slurm script execute it and remove it
+    f = open('_valid.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _valid.slurm')
+    os.system('rm _valid.slurm')
+    return None 
+
+
+
 #for i in range(5): 
-for i in [0, 3, 4]: 
-    anpe('toy', i, 500, 5)
-    time.sleep(10) 
+#    anpe('toy', i, 500, 5)
+#    time.sleep(10) 
 
 #for i in range(5): 
 #    anpe('toy', i, 100, 10)
 #    time.sleep(10) 
+
+#for i in range(5): 
+#    anpe('toy', i, 500, 10)
+#    time.sleep(10) 
+
+for i in range(5): 
+    valid('toy', i, 500, 5)
+    valid('toy', i, 100, 10)
+
+for i in [0, 3, 4]: 
+    valid('toy', i, 500, 10)
