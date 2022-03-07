e5cdfd2ed9b7d3af60af09c6d2d50d76adc40fec
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2022-03-07 11:54:33 -0500

    adding logo

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,10 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+        'logo': 'logo.png'
+        }
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
