547301c43595afc29037e119286aa3b259437dc9
Chang Hahn <changhoon.hahn@princeton.edu>
2022-04-06 13:49:49 -0400

    scripts to - sample the SDSS IVAR NDE for the noise latent variables - decode noise latent variables and apply on noiseless SEDs

--- bin/training_data/adroit.py
+++ bin/training_data/adroit.py
@@ -3,6 +3,7 @@
 python script to deploy slurm jobs for constructing training set for speculator
 
 '''
+import time 
 import os, sys 
 
 
@@ -41,6 +42,85 @@ def training_sed(ibatch, nsample=100000, ncpu=32):
     return None 
 
 
-for i in range(1, 10): 
-    training_sed(0, nsample=100000, ncpu=16)
-training_sed(101, nsample=100000, ncpu=16)
+def sample_sdssivar_nde(ibatch, isplit): 
+    ''' deploy generate_trainingdata on adroit 
+    '''
+    cntnt = '\n'.join([
+        "#!/bin/bash", 
+        "#SBATCH -J sample.noise_nde.%i.%i" % (ibatch, isplit), 
+        "#SBATCH --partition=general",
+        "#SBATCH --time=23:59:59", 
+        "#SBATCH --mem-per-cpu=12G", 
+        "#SBATCH --export=ALL", 
+        "#SBATCH --output=o/_sample.noise_nde.%i.%i.o" % (ibatch, isplit), 
+        "#SBATCH --mail-type=all", 
+        "#SBATCH --mail-user=chhahn@princeton.edu", 
+        "", 
+        'now=$(date +"%T")', 
+        'echo "start time ... $now"', 
+        "", 
+        "source ~/.bashrc", 
+        "conda activate torch-env", 
+        "",
+        "python /home/chhahn/projects/SEDflow/bin/training_data/sample_sdss_ivar_nde.py %i %i"  % (ibatch, isplit),
+        "",
+        'now=$(date +"%T")', 
+        'echo "end time ... $now"', 
+        ""]) 
+
+    # create the slurm script execute it and remove it
+    f = open('_train.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _train.slurm')
+    os.system('rm _train.slurm')
+    return None 
+
+
+def apply_sdssivar_nde(ibatch): 
+    ''' deploy generate_trainingdata on adroit 
+    '''
+    cntnt = '\n'.join([
+        "#!/bin/bash", 
+        "#SBATCH -J apply.noise_nde.%i" % (ibatch), 
+        "#SBATCH --partition=general",
+        "#SBATCH --time=23:59:59", 
+        "#SBATCH --mem-per-cpu=32G", 
+        "#SBATCH --export=ALL", 
+        "#SBATCH --output=o/_apply.noise_nde.%i.o" % (ibatch), 
+        "#SBATCH --mail-type=all", 
+        "#SBATCH --mail-user=chhahn@princeton.edu", 
+        "", 
+        'now=$(date +"%T")', 
+        'echo "start time ... $now"', 
+        "", 
+        "source ~/.bashrc", 
+        "conda activate torch-env", 
+        "",
+        "python /home/chhahn/projects/SEDflow/bin/training_data/apply_sdss_ivar_nde.py %i"  % (ibatch),
+        "",
+        'now=$(date +"%T")', 
+        'echo "end time ... $now"', 
+        ""]) 
+
+    # create the slurm script execute it and remove it
+    f = open('_train.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _train.slurm')
+    os.system('rm _train.slurm')
+    return None 
+
+
+
+#for i in range(10): 
+#    training_sed(i, nsample=100000, ncpu=16)
+#training_sed(101, nsample=100000, ncpu=16)
+
+
+# sample sdssivar NDE in 10 different chunks 
+#for i in range(10): 
+#    sample_sdssivar_nde(0, i)
+#    time.sleep(10)
+
+apply_sdssivar_nde(0)
