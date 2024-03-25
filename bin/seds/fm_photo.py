89e04b9110c0bafca009e60b56a662e7a3430161
changhoonhahn <changhoon.hahn@princeton.edu>
2024-03-25 08:40:20 -0700

    implemented photometric forward model

---
+++ bin/seds/fm_photo.py
@@ -0,0 +1,89 @@
+'''
+
+script to forward model photometry
+
+
+'''
+import os,sys
+import psutil
+import numpy as np
+import multiprocessing as mp 
+
+from speclite import filters as specFilter
+
+name    = sys.argv[1]
+seed    = int(sys.argv[2])
+band    = sys.argv[3] 
+
+n_cpu   = psutil.cpu_count(logical=False)
+dat_dir = '/pscratch/sd/c/chahah/sedflow/seds/'
+
+#############################################
+# set bandpasses 
+#############################################
+if band == 'ugrizJ': 
+    # set up photometric filters u, g, r, i, z, J
+    sdss_u = specFilter.load_filter('sdss2010-u')
+    sdss_g = specFilter.load_filter('sdss2010-g')
+    sdss_r = specFilter.load_filter('sdss2010-r')
+    sdss_i = specFilter.load_filter('sdss2010-i')
+    sdss_z = specFilter.load_filter('sdss2010-z')
+    hsc_y = specFilter.load_filter('hsc2017-y')
+
+    bandpasses = specFilter.FilterSequence([sdss_u, sdss_g, sdss_r, sdss_i, sdss_z, hsc_y])
+elif band == 'grzW1W2W3W4': 
+    # set up photometric filters DECam DR1 g, r, z with WISE W1, W2, W3, W4
+    # this is ultimately what we want to use with DESI photometry 
+    if name == 'modela': 
+        raise ValueError("Model A does not include dust emission so it cannot reliably model W3 and W4 bands")
+    decam_g = specFilter.load_filter('decamDR1-g')
+    decam_r = specFilter.load_filter('decamDR1-r')
+    decam_z = specFilter.load_filter('decamDR1-z')
+    wise_w1 = specFilter.load_filter('wise2010-W1')
+    wise_w2 = specFilter.load_filter('wise2010-W2')
+    wise_w3 = specFilter.load_filter('wise2010-W3')
+    wise_w4 = specFilter.load_filter('wise2010-W4')
+
+    bandpasses = specFilter.FilterSequence([decam_g, decam_r, decam_z,
+                                            wise_w1, wise_w2, wise_w3, wise_w4])
+elif band == 'grzW1W2': 
+    # set up photometric filters DECam DR1 g, r, z with WISE W1, W2
+    # this is probably the best we can do with Model A
+    decam_g = specFilter.load_filter('decamDR1-g')
+    decam_r = specFilter.load_filter('decamDR1-r')
+    decam_z = specFilter.load_filter('decamDR1-z')
+    wise_w1 = specFilter.load_filter('wise2010-W1')
+    wise_w2 = specFilter.load_filter('wise2010-W2')
+    bandpasses = specFilter.FilterSequence([decam_g, decam_r, decam_z,
+                                            wise_w1, wise_w2])
+else: 
+    raise ValueError("specify one of the bands") 
+
+#############################################
+# read wavelengths and SEDs
+#############################################
+wave = np.load(os.path.join(dat_dir, 'train_sed.%s.%i.waves.npz' % (name, seed)))['arr_0']
+seds = np.load(os.path.join(dat_dir, 'train_sed.%s.%i.seds.npz' % (name, seed)))['arr_0']
+
+# convert flux  units 
+fl = seds / (3.34e4) / wave**2 # ergs/s/cm^2/A
+
+#############################################
+# calcluate magnitudes 
+#############################################
+def fm_maggies(ii): 
+    # convolve redshifted SEds with bnadpasses
+    _maggies = np.array([np.array(list(arr)) 
+                         for arr in bandpasses.get_ab_maggies(fl[ii], wavelength=wave[ii]).as_array()])
+    return _maggies 
+
+
+with mp.Pool(n_cpu) as p: 
+    wfps = p.map(fm_maggies, np.arange(seds.shape[0]))
+
+maggies = np.array(wfps)
+mags = 22.5 - 2.5 * np.log10(maggies * 1e9) # convert maggies to magnitudes 
+
+# save to file 
+np.savez_compressed(os.path.join(dat_dir, 'train_sed.%s.%i.mags_%s.npz' % (name, seed, band)), 
+                    mags)
--- bin/seds/saul.py
+++ bin/seds/saul.py
@@ -7,13 +7,13 @@ script for deploying job on perlmutter
 import os, sys
 
 
-def deploy_modela(nsample, seed):
+def modela_sed(nsample, seed):
     ''' create slurm script and then submit
     '''
     cntnt = '\n'.join([
         "#!/bin/bash",
         "#SBATCH --qos=regular",
-        "#SBATCH --time=08:00:00",
+        "#SBATCH --time=01:59:59",
         "#SBATCH --constraint=cpu",
         "#SBATCH -N 1",
         "#SBATCH -J modela%i" % seed,
@@ -39,5 +39,41 @@ def deploy_modela(nsample, seed):
     return None
 
 
-for i in range(10): 
-    deploy_modela(10000, i)
+def modela_photo(seed, bands): 
+    cntnt = '\n'.join([
+        "#!/bin/bash",
+        "#SBATCH --qos=regular",
+        "#SBATCH --time=00:09:59",
+        "#SBATCH --constraint=cpu",
+        "#SBATCH -N 1",
+        "#SBATCH -J modela%i_%s" % (seed, bands),
+        "#SBATCH -o ofiles/modela%i_%s.o" % (seed, bands),
+        "",
+        'now=$(date +"%T")',
+        'echo "start time ... $now"',
+        "",
+        "source ~/.bashrc",
+        "conda activate gqp",
+        "",
+        "python /global/homes/c/chahah/projects/SEDflow/bin/seds/fm_photo.py modela %i %s" % (seed, bands),
+        'now=$(date +"%T")',
+        'echo "end time ... $now"',
+        ""])
+
+    # create the slurm script execute it and remove it
+    f = open('_train.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _train.slurm')
+    os.system('rm _train.slurm')
+    return None
+
+
+
+# run 2024,03,22
+#for i in range(10): 
+#    modela_sed(100000, i)
+
+for i in range(1, 10): 
+    modela_photo(i, 'grzW1W2') 
+    modela_photo(i, 'ugrizJ') 
