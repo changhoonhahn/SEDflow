3ff05fcb9a7b28d992637e831b4e29d1c1ac3f3b
changhoonhahn <changhoon.hahn@princeton.edu>
2024-03-27 06:58:33 -0700

    best nde selection implemented

--- src/sedflow/util.py
+++ src/sedflow/util.py
@@ -4,6 +4,11 @@ utility functions
 
 '''
 import os
+import glob
+import numpy as np 
+
+import torch
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 
 def data_dir(): 
@@ -17,3 +22,36 @@ def data_dir():
             ]
     for _dir in dat_dirs: 
         if os.path.isdir(_dir): return _dir
+
+
+def read_best_qphi(study_name, n_ensemble=5, device='cpu', name='modela'): 
+    dat_dir = os.path.join(data_dir(), 'qphi', name) 
+
+    fevents = glob.glob(os.path.join(dat_dir, '%s/*/events*' % study_name))
+
+    events, best_valid = [], []
+    for fevent in fevents: 
+        ea = EventAccumulator(fevent)
+        ea.Reload()
+
+        try: 
+            best_valid.append(ea.Scalars('best_validation_log_prob')[0].value)
+            events.append(fevent)
+        except: 
+            pass #print(fevent)
+
+    best_valid = np.array(best_valid)
+    print('%i models trained' % np.max([int(os.path.dirname(event).split('.')[-1]) 
+        for event in events]))
+    
+    i_models = [int(os.path.dirname(events[i]).split('.')[-1]) for i 
+            in np.argsort(best_valid)[-n_ensemble:][::-1]]
+    print(i_models) 
+    
+    qphis = []
+    for i_model in i_models: 
+        fqphi = os.path.join(dat_dir, '%s/%s.%i.pt' % (study_name, study_name, i_model))
+        qphi = torch.load(fqphi, map_location=device)
+        qphis.append(qphi)
+
+    return qphis
