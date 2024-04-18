858854de3a407e6aa2eb950b7a8804dcc6aeb1e2
changhoonhahn <changhoon.hahn@princeton.edu>
2024-04-18 14:15:44 -0700

    implementing forward models for Model B

--- bin/models/fm_photo.py
+++ bin/models/fm_photo.py
@@ -16,7 +16,7 @@ seed    = int(sys.argv[2])
 band    = sys.argv[3] 
 
 n_cpu   = psutil.cpu_count(logical=False)
-dat_dir = '/pscratch/sd/c/chahah/sedflow/seds/modela/'
+dat_dir = os.path.join(U.data_dir(), 'seds', name)
 
 #############################################
 # set bandpasses 
--- bin/models/saul.py
+++ bin/models/saul.py
@@ -101,7 +101,38 @@ def modela_anpe(seed, bands):
     os.system('sbatch _train.slurm')
     os.system('rm _train.slurm')
     return None
+ 
 
+def modelb_sed(nsample, seed):
+    ''' create slurm script and then submit
+    '''
+    cntnt = '\n'.join([
+        "#!/bin/bash",
+        "#SBATCH --qos=debug",
+        "#SBATCH --time=00:29:59",
+        "#SBATCH --constraint=cpu",
+        "#SBATCH -N 1",
+        "#SBATCH -J modela%i" % seed,
+        "#SBATCH -o ofiles/modela%i.o" % seed,
+        "",
+        'now=$(date +"%T")',
+        'echo "start time ... $now"',
+        "",
+        "source ~/.bashrc",
+        "conda activate gqp",
+        "",
+        "python /global/homes/c/chahah/projects/SEDflow/bin/modelb/modelb.py %i %i" % (nsample, seed),
+        'now=$(date +"%T")',
+        'echo "end time ... $now"',
+        ""])
+
+    # create the slurm script execute it and remove it
+    f = open('_train.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _train.slurm')
+    os.system('rm _train.slurm')
+    return None
 
 
 # run 2024,03,22
@@ -116,7 +147,5 @@ def modela_anpe(seed, bands):
 #modela_photo(999, 'grzW1W2') 
 #modela_photo(999, 'ugrizJ') 
 
-for i in range(10): 
-    modela_anpe(i, 'grzW1W2')
-    modela_anpe(i, 'ugrizJ')
-
+# run 2024.04.18
+modelb_sed(1000, 0)
