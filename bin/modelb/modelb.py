802b1086ac6d9cf1c0fb73d32c954a10f998f420
changhoonhahn <changhoon.hahn@princeton.edu>
2024-04-18 16:53:25 -0400

    model b

---
+++ bin/modelb/modelb.py
@@ -0,0 +1,73 @@
+'''
+
+script to generate training SED data for the SEDflow project
+
+'''
+import os,sys
+import psutil
+import numpy as np
+import multiprocessing as mp 
+# --- provabgs --- 
+from provabgs import infer as Infer
+from provabgs import models as Models
+# -- SEDflow --- 
+from sedflow import util as U
+
+####################################################
+# input 
+####################################################
+Nsample = int(sys.argv[1])
+seed    = int(sys.argv[2])
+n_cpu   = psutil.cpu_count(logical=False)
+dat_dir = '/pscratch/sd/c/chahah/sedflow/seds/'
+####################################################
+print('using %i CPUs to run %i SEDs' % (n_cpu, Nsample))
+
+# SPS parameter priors 
+prior_sps = Infer.load_priors([
+    Infer.UniformPrior(7., 13., label='sed'),
+    Infer.FlatDirichletPrior(4, label='sed'),           # flat dirichilet priors
+    Infer.UniformPrior(0., 1., label='sed'),            # burst fraction
+    Infer.UniformPrior(1e-2, 13.27, label='sed'),       # tburst
+    Infer.LogUniformPrior(4.5e-5, 1.5e-2, label='sed'), # log uniform priors on ZH coeff
+    Infer.LogUniformPrior(4.5e-5, 1.5e-2, label='sed'), # log uniform priors on ZH coeff
+    Infer.UniformPrior(0., 3., label='sed'),            # uniform priors on dust1
+    Infer.UniformPrior(0., 3., label='sed'),            # uniform priors on dust2
+    Infer.UniformPrior(-2., 1., label='sed'),           # uniform priors on dust_index
+    Infer.UniformPrior(0.1, 15., label='sed'),          # uniform priors on Umin based on Leja+(2017)
+    Infer.UniformPrior(0.0, 0.15, label='sed'),         # uniform priors on gamma_e based on Leja+(2017)
+    Infer.UniformPrior(0.1, 0.7, label='sed')           # uniform priors on Q_PAH based on Leja+(2017)
+])
+
+# SPS model 
+m_sps = Models.NMF_NDE(burst=True, emulator=False)
+m_sps._ssp_initiate()
+
+# draw (parameters, z) from the prior
+print('Drawing theta_SPS and redshifts') 
+np.random.seed(seed)
+
+thetas_sps  = np.array([prior_sps.transform(prior_sps.sample()) for i in range(Nsample)])
+thetas_unt  = prior_sps.untransform(thetas_sps)
+redshifts   = np.random.uniform(0, 1., Nsample) 
+thetas      = np.concatenate([thetas_sps, np.atleast_2d(redshifts).T], axis=1)
+
+####################################################
+# generate SED for (parameters, z) values 
+####################################################
+print('Generating SED(theta_SPS, redshifts)') 
+def SED(tt): 
+    w, f = m_sps.sed(tt[:-1], tt[-1]) 
+    return [w, f]
+
+with mp.Pool(n_cpu) as p: 
+    wfps = p.map(SED, thetas)
+
+waves   = np.array([wfp[0] for wfp in wfps])
+seds    = np.array([wfp[1] for wfp in wfps]) 
+
+np.savez_compressed(os.path.join(dat_dir, 'modelb', 'train_sed.modelb.%i.thetas_sps.npz' % seed), thetas_sps)
+np.savez_compressed(os.path.join(dat_dir, 'modelb', 'train_sed.modelb.%i.thetas_unt.npz' % seed), thetas_unt)
+np.savez_compressed(os.path.join(dat_dir, 'modelb', 'train_sed.modelb.%i.redshifts.npz' % seed), redshifts)
+np.savez_compressed(os.path.join(dat_dir, 'modelb', 'train_sed.modelb.%i.waves.npz' % seed), waves)
+np.savez_compressed(os.path.join(dat_dir, 'modelb', 'train_sed.modelb.%i.seds.npz' % seed), seds)
--- src/sedflow/util.py
+++ src/sedflow/util.py
@@ -7,6 +7,8 @@ import os
 import glob
 import numpy as np 
 
+from scipy.special import erf, erfinv
+
 import torch
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
@@ -55,3 +57,64 @@ def read_best_qphi(study_name, n_ensemble=5, device='cpu', name='modela'):
         qphis.append(qphi)
 
     return qphis
+
+
+def read_best_fqphi(study_name, n_ensemble=5, name='modela'): 
+    dat_dir = os.path.join(data_dir(), 'qphi', name) 
+
+    fevents = glob.glob(os.path.join(dat_dir, '%s/*/events*' % study_name))
+
+    events, best_valid = [], []
+    for fevent in fevents: 
+        ea = EventAccumulator(fevent)
+        ea.Reload()
+
+        try: 
+            best_valid.append(ea.Scalars('best_validation_log_prob')[0].value)
+            events.append(fevent)
+        except: 
+            pass #print(fevent)
+
+    best_valid = np.array(best_valid)
+    print('%i models trained' % np.max([int(os.path.dirname(event).split('.')[-1]) 
+        for event in events]))
+    
+    i_models = [int(os.path.dirname(events[i]).split('.')[-1]) for i 
+            in np.argsort(best_valid)[-n_ensemble:][::-1]]
+    print(i_models) 
+    
+    fqphis = []
+    for i_model in i_models: 
+        fqphis.append(os.path.join(dat_dir, '%s/%s.%i.pt' % (study_name, study_name, i_model)))
+    
+    return fqphis
+
+
+def cdf_transform(x, bounds):
+    """ Transform from a Gaussian (which is x) to a Uniform with bounds as input.
+    """
+    return _gaussian_cdf(x, 0, 1) * (bounds[1] - bounds[0]) + bounds[0]
+
+
+def inv_cdf_transform(x, bounds):
+    """ Transform from a Uniform with bounds (which is x) to a Gaussian
+    """
+    return _inv_gaussian_cdf((x - bounds[0]) / (bounds[1] - bounds[0]), 0, 1)
+
+
+
+def _gaussian_cdf(x, mu, sigma):
+    """ CDF of a Gaussian distribution.
+
+    :math:`F(x) = \\frac{1}{2}(1 + erf(\\frac{x - \\mu}{\\sigma}))`
+    """
+    return 0.5 * (1 + erf((x - mu) / (np.sqrt(2) * sigma)))
+
+
+def _inv_gaussian_cdf(x, mu, sigma):
+    """ Inverse CDF of a Gaussian distribution.
+
+    :math:`F^{-1}(x) = \\mu + \\sigma \\sqrt{2} \\text{erfinv}(2 \\times x - 1)`
+
+    """
+    return mu + sigma * np.sqrt(2) * erfinv(2 * x - 1)
