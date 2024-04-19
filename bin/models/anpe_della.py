442f8adf5f59f9a5e24abb960ef9afaf538da0f6
changhoonhahn <changhoon.hahn@princeton.edu>
2024-04-19 09:11:31 -0400

    anpe training and validation on della

---
+++ bin/models/anpe_della.py
@@ -0,0 +1,124 @@
+'''
+
+
+script to train Neural Posterior Estimators with Optuna hyperparameter optimization on della 
+with the updated framework
+
+
+'''
+import os, sys 
+import numpy as np
+
+import torch
+from torch import nn 
+from torch.utils.tensorboard.writer import SummaryWriter
+
+import optuna 
+
+from sbi import utils as Ut
+from sbi import inference as Inference
+
+from sedflow import data as D
+from sedflow import util as U
+from sedflow import flows as F
+
+##################################################################################
+# input 
+##################################################################################
+bands = sys.argv[1] # which bands passes
+freez = (sys.argv[2] == True) 
+try: 
+    study = sys.argv[3]
+except: 
+    study = ''
+##################################################################################
+cuda = torch.cuda.is_available()
+device = ("cuda:0" if cuda else "cpu")
+
+seed = 12387
+torch.manual_seed(seed)
+if cuda:
+    torch.cuda.manual_seed(seed)
+##################################################################################
+# load data 
+##################################################################################
+theta_train, X_train = D.load_modela('train', bands=bands, infer_redshift=freez)
+
+print('Ntrain + Nvalid = %i' % (theta_train.shape[0]))
+
+##################################################################################
+# set prior range
+##################################################################################
+if not freez: 
+    prior_low   = [7, 0., 0., 0., 0., 1e-2, np.log10(4.5e-5), np.log10(4.5e-5), 0, 0., -2.]
+    prior_high  = [13.0, 1., 1., 1., 1., 13.27, np.log10(1.5e-2), np.log10(1.5e-2), 3., 3., 1.]
+else: 
+    prior_low   = [7, 0., 0., 0., 0., 1e-2, np.log10(4.5e-5), np.log10(4.5e-5), 0, 0., -2., 0]
+    prior_high  = [13.0, 1., 1., 1., 1., 13.27, np.log10(1.5e-2), np.log10(1.5e-2), 3., 3., 1., 1.]
+assert len(prior_low) == theta_train.shape[1], "prior and data dimension mismatch"  
+
+flo = F.Flow(device=device) 
+flo.set_prior('uniform', low=prior_low, high=prior_high)
+##################################################################################
+# OPTUNA
+##################################################################################
+output_dir = os.path.join(U.data_dir(), 'qphi', 'modela') 
+
+n_trials    = 1000
+if not freez: 
+    study_name  = 'qphi.modela%s.%s.theta.nmgysigz' % (study, bands)
+else: 
+    study_name  = 'qphi.modela%s.%s.thetaz.nmgysig' % (study, bands)
+
+n_jobs     = 1
+if not os.path.isdir(os.path.join(output_dir, study_name)): 
+    os.system('mkdir %s' % os.path.join(output_dir, study_name))
+storage    = 'sqlite:///%s/%s/%s.db' % (output_dir, study_name, study_name)
+n_startup_trials = 20
+
+n_blocks_min, n_blocks_max = 2, 10
+n_transf_min, n_transf_max = 2, 10
+n_hidden_min, n_hidden_max = 64, 512
+p_drop_min, p_drop_max = 0., 1.
+
+
+def Objective(trial):
+    ''' bojective function for optuna 
+    '''
+    # Generate the model                                         
+    n_blocks = trial.suggest_int("n_blocks", n_blocks_min, n_blocks_max)
+    n_transf = trial.suggest_int("n_transf", n_transf_min,  n_transf_max)
+    n_hidden = trial.suggest_int("n_hidden", n_hidden_min, n_hidden_max, log=True)
+    p_drop = trial.suggest_float("p_drop", p_drop_min, p_drop_max)
+    
+
+    nde = flo.nde(
+            architecture='maf', 
+            n_hidden=n_hidden, 
+            n_transf=n_transf, 
+            n_blocks=n_blocks, 
+            p_drop=p_drop, 
+            use_batch_norm=True)
+
+    flo.train_flow(theta_train, X_train, 
+            nde=nde, # nde we're training
+            summary_writer=SummaryWriter('%s/%s/%s.%i' % (output_dir, study_name, study_name, trial.number)), 
+            training_batch_size=500)
+    best_valid_log_prob = flo.anpe._summary['best_validation_log_prob'][0]
+    flo.anpe._summary_writer.add_hparams(
+            {'n_blocks': n_blocks, 'n_transf': n_transf, 'n_hidden': n_hidden, 'p_drop': p_drop},
+            {'best_valid_log_prob': best_valid_log_prob}
+            )
+
+    # save trained flow
+    fqphi   = os.path.join(output_dir, study_name, '%s.%i.pt' % (study_name, trial.number))
+    torch.save(flo.flow, fqphi)
+        
+    return -1*best_valid_log_prob
+
+
+sampler     = optuna.samplers.TPESampler(n_startup_trials=n_startup_trials)
+study       = optuna.create_study(study_name=study_name, sampler=sampler, storage=storage, directions=["minimize"], load_if_exists=True) # , "minimize", "minimize"
+
+study.optimize(Objective, n_trials=n_trials, n_jobs=n_jobs)
+print("  Number of finished trials: %i" % len(study.trials))
---
+++ bin/models/della.py
@@ -0,0 +1,49 @@
+'''
+
+python script for deploying job on adroit 
+
+'''
+import os, sys
+import time 
+
+
+def anpe(seed, bands, study, gpu=False):
+    ''' deploy ANPE training 
+    '''
+    cntnt = '\n'.join([
+        "#!/bin/bash", 
+        "#SBATCH -J modela%s%i_%s_anpe%s" % (study, seed, bands, ['', '_gpu'][gpu]),
+        "#SBATCH --time=95:59:59", 
+        #"#SBATCH --time=00:29:59", 
+        "#SBATCH --export=ALL", 
+        "#SBATCH -o ofiles/modela%s%i_%s_anpe%s.o" % (study, seed, bands, ['', '_gpu'][gpu]),
+        "#SBATCH --mail-type=all", 
+        "#SBATCH --mail-user=chhahn@princeton.edu", 
+        ["", "#SBATCH --gres=gpu:1"][gpu], 
+        ['', "#SBATCH --mem-per-cpu=8G"][gpu], 
+        "", 
+        'now=$(date +"%T")', 
+        'echo "start time ... $now"', 
+        "", 
+        "source ~/.bashrc", 
+        "conda activate sbi", 
+        "",
+        "python /home/chhahn/projects/SEDflow/bin/modela/anpe_della.py  %s False %s" % (bands, study), 
+        "",
+        'now=$(date +"%T")', 
+        'echo "end time ... $now"', 
+        ""]) 
+
+    # create the slurm script execute it and remove it
+    f = open('_anpe.slurm','w')
+    f.write(cntnt)
+    f.close()
+    os.system('sbatch _anpe.slurm')
+    os.system('rm _anpe.slurm')
+    return None 
+
+for i in range(10): 
+    anpe(i, 'ugrizJ', '.cdf', gpu=False)
+    anpe(i, 'grzW1W2', '.cdf', gpu=False)
+    anpe(i, 'ugrizJ', '.cdf', gpu=True)
+    anpe(i, 'grzW1W2', '.cdf', gpu=True)
