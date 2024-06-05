437934eccecb086667b1436db695691c05ff5f7c
changhoonhahn <changhoon.hahn@princeton.edu>
2024-06-05 02:28:58 -0700

    model c loader added

--- bin/models/util.py
+++ bin/models/util.py
@@ -113,6 +113,54 @@ def load_modelb(train_or_test, bands='grzW1W2', infer_redshift=False):
     return x, y
 
 
+def load_modelc(train_or_test, bands='grzW1W2', infer_redshift=False): 
+    ''' load training/testing data for SEDflow Model B
+
+    Returns
+    -------
+    x, y for p(x|y) 
+    '''
+    if bands not in ['grzW1W2', 'ugrizJ']: raise ValueError
+    if train_or_test == 'train': 
+        seeds = np.arange(10) 
+    elif train_or_test == 'test': 
+        seeds = [999]
+    else: 
+        raise ValueError
+    
+    dat_dir = os.path.join(data_dir(), 'seds') 
+
+    theta, zred, nmgy, sigs = [], [], [], []
+    for seed in seeds: 
+        _theta = np.load(os.path.join(dat_dir, 'modelc', 'train_sed.modelc.%i.thetas_unt.npz' % seed))['arr_0']
+        _zred = np.load(os.path.join(dat_dir, 'modelc', 'train_sed.modelc.%i.redshifts.npz' % seed))['arr_0']
+        _nmgy = np.load(os.path.join(dat_dir, 'modelc', 'train_sed.modelc.%i.nmgy_noisy_%s.npy' % (seed, bands)))
+        _sigs = np.load(os.path.join(dat_dir, 'modelc', 'train_sed.modelc.%i.sig_nmgy_%s.npy' % (seed, bands)))
+        
+        theta.append(_theta)
+        zred.append(_zred)
+        nmgy.append(_nmgy)
+        sigs.append(_sigs)
+
+    theta = np.concatenate(theta)
+    # put metallicities in log10 space 
+    theta[:,6] = np.log10(theta[:,6])
+    theta[:,7] = np.log10(theta[:,7])
+
+    zred = np.concatenate(zred) 
+    nmgy = np.log10(np.concatenate(nmgy)) # convert to log-nanomaggies to reduce dynamic range 
+    sigs = np.concatenate(sigs)
+
+
+    if not infer_redshift: 
+        x = theta
+        y = np.concatenate([nmgy, sigs, zred[:,None]], axis=1) 
+    else: 
+        x = np.concatenate([theta, zred[:,None]], axis=1)
+        y = np.concatenate([nmgy, sigs], axis=1) 
+    return x, y
+
+
 def data_dir(): 
     ''' get main data directory where the files are stored for whichever machine I'm on 
     '''
